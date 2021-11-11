0d1223bb2ec13cb00e0ff632357f5d170e029fc4
iron72 <iron72rus@gmail.com>
2021-11-11 12:01:01 +0300

    Cycles Test

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,4 +1,14 @@
 package ru.netology.sqr;
 
 public class SQRService {
+
+    public int CyclesHomeWork1 (int limitLow, int limitUp) {
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= limitLow && i * i <= limitUp) {
+                counter = counter + 1;
+            }
+        }
+        return counter;
+    }
 }
